70dbe06ef65ce3758121ef5da452034f95d272a2
jasecoop <jase@jasecoop.com>
2017-08-06 22:51:11 +0100

    zone2

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,127 @@
-// Gulp tasks for JKL Tachyons
+var paths = {};
+
+var siteRoot = '_site';
+// Directory locations
+var appDir             = '_app/';  // The files Gulp will work on
+var jekyllDir          = '';       // The files Jekyll will work on
+var siteDir            = '_site/'; // The resulting static site
+
+// Folder naming conventions
+var postFolderName   = '_posts';
+var draftFolderName  = '_drafts';
+var imageFolderName  = 'images';
+var fontFolderName   = 'fonts';
+var scriptFolderName = 'js';
+var stylesFolderName = 'css';
+
+// Glob patterns by file type
+var cssPattern         = '/**/*.css';
+var jsPattern          = '/main.js';
+var imagePattern       = '/**/*.+(jpg|JPG|jpeg|JPEG|png|PNG|svg|SVG|gif|GIF|webp|WEBP|tif|TIF|mp4)';
+var markdownPattern    = '/**/*.+(md|MD|markdown|MARKDOWN)';
+var htmlPattern        = '/**/*.html';
+var xmlPattern         = '/**/*.xml';
+var fontPatterns       = '/**.*';
+
+
 
 // Load plugins
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    basswork = require('gulp-basswork'),
     watch = require('gulp-watch'),
-    prefix = require('gulp-autoprefixer'),
-    //uncss = require('gulp-uncss'),
-    minifyCSS = require('gulp-minify-css'),
-    //sass = require('gulp-sass'),
-    size = require('gulp-size'),
-    rename = require('gulp-rename'),
-    csslint = require('gulp-csslint'),
+    include = require('gulp-include'),
     css = require('css'),
     browserSync = require('browser-sync'),
-    browserReload = browserSync.reload;
+    browserReload = browserSync.reload,
+    child = require('child_process'),
+    postcss = require('gulp-postcss'),
+    cssvariables = require('postcss-css-variables'),
+    atImport = require("postcss-import"),
+    customMedia = require("postcss-custom-media"),
+    include = require("gulp-include"),
 
-gulp.task('css', function() {
-  gulp.src('./src/jkl-tachyons.css')
-    .pipe(basswork())
-    .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
-    .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
-    .pipe(gulp.dest('./css'))
-    .pipe(minifyCSS())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(size({gzip: false, showFiles: true, title:'basswork minified'}))
-    .pipe(size({gzip: true, showFiles: true, title:'basswork minified'}))
-    .pipe(gulp.dest('./css'));
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    transform = require('vinyl-transform'),
+    source = require('vinyl-source-stream'),
+    streamify = require('gulp-streamify'),
+    rename = require('gulp-rename'),
+    connect = require('gulp-connect'),
+    runSequence  = require('run-sequence'),
+    debug = require('gulp-debug'),
+    concat = require('gulp-concat'),
+    size = require('gulp-size'),
+    run = require('gulp-run');
+
+
+
+// Uses Sass compiler to process styles, adds vendor prefixes, minifies,
+// and then outputs file to appropriate location(s)
+gulp.task('build:styles', function() {
+  var processors = [
+      atImport(),
+      customMedia(),
+      cssvariables()
+  ];
+  gulp.src('src/tachyons.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('_site/assets/css'))
+    .on('error', gutil.log);
 });
 
-// Initialize browser-sync which starts a static server also allows for
-// browsers to reload on filesave
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "./_site/"
-        }
-    });
+
+// Concatenates and uglifies JS files and outputs result to
+// the appropriate location(s).
+// gulp.task('build:scripts', function() {
+//   var bundleStream = browserify({entries: '_app/js/main.js'});
+
+//   return bundleStream.bundle()
+//     .on('error', function (err) {
+//         console.log(err.toString());
+//         this.emit("end");
+//     })
+//     .pipe(source("_app/js/main.js"))
+//     .pipe(rename('main.js'))
+//     .pipe(gulp.dest('assets/js'))
+// });
+
+// Runs Jekyll build
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
-// Function to call for reloading browsers
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch('_app/css/**/*.css', ['build:styles']);
+  // gulp.watch('_app/js/**/*.js', ['build:scripts']);
+  gulp.watch(['_app/images/**/*'], ['build:images']);
+
+  // Watch Jekyll html files
+  gulp.watch(['*.html', '_includes/**/*.html'], ['jekyll']);
+
 });
 
-/*
-   DEFAULT TASK
-
- • Process sass then auto-prefixes and lints outputted css
- • Starts a server on port 3000
- • Reloads browsers when you change html or sass files
-
-*/
-gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
-  gulp.start(['css', 'bs-reload']);
-  gulp.watch('src/*', ['css']);
-  gulp.watch(['*.html', './**/*.html'], ['bs-reload']);
-});
+gulp.task('default', ['build:styles', 'jekyll', 'serve']);
+gulp.task('build', ['build:styles', 'jekyll']);
 
