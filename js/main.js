809f262d80fab9bb247199735bf9cb3c52d027b0
jasecoop <jase@jasecoop.com>
2017-08-21 21:32:55 +0100

    push

--- js/main.js
+++ js/main.js
@@ -3,4 +3,26 @@ $(document).ready(function() {
     e.preventDefault();
     $('.c-form').addClass('c-form-active');
   });
+
+
+  $('#emailForm').submit(function(event) {
+      event.preventDefault();
+
+      var formEl = $(this);
+      var submitButton = $('button', formEl);
+
+      $.ajax({
+        type: 'POST',
+        url: formEl.prop('action'),
+        accept: {
+          javascript: 'application/javascript'
+        },
+        data: formEl.serialize(),
+        beforeSend: function() {
+
+        }
+        }).done(function(data) {
+          $('#submitButton').html("WE'LL BE IN TOUCH").addClass('submitted')
+        });
+    });
 });
